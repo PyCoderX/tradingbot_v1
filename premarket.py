bce99e425374b560a3da2007b25b89e046d18bbd
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-11-07 08:48:30 +0530

    Update premarket.py

--- premarket.py
+++ premarket.py
@@ -2,10 +2,18 @@ from technicalanalysis import TechnicalAnalysis
 from symbollist import SymbolList
 import datetime
 import pandas as pd
+from functools import lru_cache
+import concurrent.futures
 
 
 class PreMarketAnalysis(TechnicalAnalysis):
     def __init__(self, FyersClient):
+        """Initialize the PreMarketAnalysis object.
+
+        Args:
+            FyersClient: The FyersClient object used for accessing Fyers API.
+
+        """
         TechnicalAnalysis.__init__(self)
         self.FyersClient = FyersClient
 
@@ -17,13 +25,29 @@ class PreMarketAnalysis(TechnicalAnalysis):
         )
 
     def _getSymbols(self, index: str) -> list:
+        """Get the list of symbols for the given index.
+
+        Args:
+            index: The index for which to retrieve the symbols.
+
+        Returns:
+            list: The list of symbols.
+
+        """
         return SymbolList(index=index).__call__()
 
     def _historicalData(self, index: str):
-        # get symbol list
+        """Retrieve historical data for the given index.
+
+        Args:
+            index: The index for which to retrieve the historical data.
+
+        Returns:
+            list: The historical data for each symbol.
+
+        """
         symbol_list = self._getSymbols(index)
 
-        # Calculate date range for historical data
         end_date = datetime.datetime.now() + datetime.timedelta(days=-1)
         start_date = end_date + datetime.timedelta(days=-100)
 
@@ -34,9 +58,21 @@ class PreMarketAnalysis(TechnicalAnalysis):
         }
         input_data = [data | {"symbol": symbol} for symbol in symbol_list]
         del data
-        return [self.FyersClient.history_daily(data) for data in input_data]
 
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            return list(executor.map(self.FyersClient.history_daily, input_data))
+
+    @lru_cache(maxsize=100)
     def get_watchlist(self, index: str):
+        """Get the watchlist for the given index.
+
+        Args:
+            index: The index for which to retrieve the watchlist.
+
+        Returns:
+            pandas.DataFrame: The watchlist containing symbol pivot data.
+
+        """
         hist_data = self._historicalData(index)
         symbol_pivot = [
             self.calculate_fibonacci_pivots(data, self.cash) for data in hist_data
