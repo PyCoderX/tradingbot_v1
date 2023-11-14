1d186efa8061ee9e8ace24fece29dbe9dfacee4e
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-11-14 10:25:23 +0530

    Update telegram_credentials.py

--- src/cred/telegram_credentials.py
+++ src/cred/telegram_credentials.py
@@ -1,9 +1,13 @@
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class TelegramCredentials:
-    token: str = os.environ.get("telegram_token")
-    group_chat_id: str = os.environ.get("telegram_group_chat_id")
-    personal_chat_id: str = os.environ.get("telegram_personal_chat_id")
+    token: str = field(default=os.environ.get("telegram_token"), repr=False)
+    group_chat_id: str = field(
+        default=os.environ.get("telegram_group_chat_id"), repr=False
+    )
+    personal_chat_id: str = field(
+        default=os.environ.get("telegram_personal_chat_id"), repr=False
+    )
