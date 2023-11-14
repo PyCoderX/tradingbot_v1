f6d452c6442aa127965ba2a2ae9fcd10e45beb31
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-11-14 10:10:31 +0530

    Update read_url.py

--- src/utils/read_url.py
+++ src/utils/read_url.py
@@ -1,11 +1,29 @@
 import io
+from functools import lru_cache, wraps
+
 import pandas as pd
 import requests
-from functools import lru_cache
-import utils
+from frozendict import frozendict
 
 
-@utils.freezeargs
+def freezeargs(func):
+    """
+    Transform mutable dictionnary  Into immutable
+    Useful to be compatible with cache
+    """
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        args = tuple(frozendict(arg) if isinstance(arg, dict) else arg for arg in args)
+        kwargs = {
+            k: frozendict(v) if isinstance(v, dict) else v for k, v in kwargs.items()
+        }
+        return func(*args, **kwargs)
+
+    return wrapped
+
+
+@freezeargs
 @lru_cache(maxsize=10)
 def read_url(url, headers=None, columns=None):
     """
