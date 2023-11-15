6bf1d5cfebddf70402dc5995a0791c39b8911bb2
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-11-15 08:43:48 +0530

    Update trading_day_checker.py

--- trading_day_checker.py
+++ trading_day_checker.py
@@ -2,29 +2,25 @@ import datetime
 import os
 import sys
 import time
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
 
 import pandas as pd
 
 import instances
 from nseapi import NSEAPI
+from utils import retry
 
 
+@dataclass
 class TradingDayChecker:
-    def __init__(self):
-        # * Initialization of telegram bot and Fyers Client
-        # * Set log and data directory
-        self.log_dir = Path.joinpath(Path.cwd(), "log")
-        self.data_dir = Path.joinpath(Path.cwd(), "data")
+    log_dir: str
+    data_dir: str
+    teleBot: Any
 
-        for dir in [self.log_dir, self.data_dir]:
-            Path.mkdir(dir, exist_ok=True)
-
-        # * Initialize telegram Bot and Fyers API
-        teleBot, FyersClient = instances.get_instance(logdir=self.log_dir)
-
-        teleBot.sendMessage(FyersClient.access_token)
-
+    @retry(max_attempts=5, initial_delay=3, backoff_factor=5)
+    def __post_init__(self):
         # * Is it a holiday or a weekend?
         today = datetime.datetime.now().date()
 
@@ -38,10 +34,11 @@ class TradingDayChecker:
         # * Check if today is a trading holiday or not
         is_off_day = today.strftime("%d-%b-%Y") in off_days or today.weekday() in {5, 6}
         if is_off_day:
-            teleBot.sendMessage(
+            self.teleBot.sendMessage(
                 "Today is a designated non-trading day. We recommend shutting down the system."
             )
             if sys.platform == "linux":
                 time.sleep(120)
                 os.system("shutdown -h now")
                 time.sleep(120)
+        
